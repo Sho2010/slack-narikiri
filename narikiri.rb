a04a00af9f3014e2da6032e429033dee27fe8671
Sho2010 <Sho2010@naitou-no-MacBook-Pro.local>
2015-12-11 04:17:42 +0900

    response content-type application/json header

--- narikiri.rb
+++ narikiri.rb
@@ -3,6 +3,9 @@ require 'sinatra/reloader'
 require 'json'
 
 post '/narikiri' do
+
+  headers 'content-type' => 'application/json'
+
   emoji, message = params[:text].partition(' ').values_at(0,2)
   {
     response_type: "in_channel",
