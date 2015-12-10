5c3cb0e3c8c2d04d4fbbcff499e75fd43a2ad3c0
Sho2010 <Sho2010@naitou-no-MacBook-Pro.local>
2015-12-11 04:05:46 +0900

    add response_type

--- narikiri.rb
+++ narikiri.rb
@@ -5,8 +5,8 @@ require 'json'
 post '/narikiri' do
   emoji, message = params[:text].partition(' ').values_at(0,2)
   {
+    response_type: "in_channel",
     emoji: emoji, 
     text: message, 
-    channel: 'in_channel'
   }.to_json
 end
