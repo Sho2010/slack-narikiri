402689065dbc22ee87087cccf8d5b0d3333e8c1d
sho naito <sho20100@gmail.com>
2016-02-24 02:18:37 +0900

    Add narikiri message in message API

--- narikiri.rb
+++ narikiri.rb
@@ -1,17 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
+require 'slack'
+
+slack = Slack::Client.new token: ENV['SLACK_TOKEN']
 
 post '/narikiri' do
-
   headers 'content-type' => 'application/json'
 
   emoji, message = params[:text].partition(' ').values_at(0,2)
   emoji.delete!(':')
+  slack.chat_postMessage(channel: params[:channel_id], text: message, username: emoji, icon_emoji: ":#{emoji}:")
 
-  {
-    response_type: "in_channel",
-    icon_emoji: ":#{emoji}:",
-    text: message, 
-  }.to_json
+  status 200
 end
