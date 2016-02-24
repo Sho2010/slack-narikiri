583a5e4fbc64bfe06534b323a3109af03d8608d1
sho naito <sho20100@gmail.com>
2016-02-24 12:22:15 +0900

    Verify token parameter

--- narikiri.rb
+++ narikiri.rb
@@ -2,10 +2,23 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'slack'
+require 'active_support'
+require 'active_support/core_ext'
+
+%w(SLACK_TOKEN VERIFICATION_TOKEN).each do |env|
+  raise Exception.new("Please set #{env} environment") if ENV[env].blank?
+end
 
 slack = Slack::Client.new token: ENV['SLACK_TOKEN']
 
+def token_valid?(params)
+  params[:token] == ENV['VERIFICATION_TOKEN']
+end
+end
+
 post '/narikiri' do
+  return status 403 unless token_valid?(params)
+
   headers 'content-type' => 'application/json'
 
   emoji, message = params[:text].partition(' ').values_at(0,2)
