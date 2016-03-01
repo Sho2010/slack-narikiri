1e51e08777c10fe7cf3677e79f31bd4d50df2adb
sho naito <sho20100@gmail.com>
2016-03-01 17:20:21 +0900

    Implement write log channel.

--- narikiri.rb
+++ narikiri.rb
@@ -14,6 +14,11 @@ slack = Slack::Client.new token: ENV['SLACK_TOKEN']
 def token_valid?(params)
   params[:token] == ENV['VERIFICATION_TOKEN']
 end
+
+def post_log_message(slack, params)
+  return if ENV['NARIKIRI_LOG_CHANNEL'].blank?
+  log = "channel:`#{params[:channel_name]}` , user:`#{params[:user_name]}` , message:`#{params[:text]}`"
+  slack.chat_postMessage(channel: ENV['NARIKIRI_LOG_CHANNEL'], text: log, username: params[:user_name])
 end
 
 post '/narikiri' do
@@ -24,6 +29,8 @@ post '/narikiri' do
   emoji, message = params[:text].partition(' ').values_at(0,2)
   emoji.delete!(':')
   slack.chat_postMessage(channel: params[:channel_id], text: message, username: emoji, icon_emoji: ":#{emoji}:")
+
+  post_log_message(slack, params)
 
   status 200
 end
